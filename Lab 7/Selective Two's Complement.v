66864804acbf053891337479c9c15a3e11acdd39
Rohan Raj <rohan.raaj2@gmail.com>
2023-04-06 01:09:15 +0500

    Create Selective Two's Complement.v

---
+++ Lab 7/Selective Two's Complement.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+module complement(
+  input O, 
+  input [2:0] B, 
+  output [2:0] C
+  ); 
+  // Write your code here 
+  assign C[0] = B[0];
+  assign C[1] = (~O & B[1]) || (B[1] & ~B[0]) || (O & ~B[1] & B[0]);
+  assign C[2] = (~O & B[2]) || (B[2] & ~B[1] & ~B[0]) || (O & ~B[2] & B[0]) || (O & ~B[2] & B[1]);
+endmodule
